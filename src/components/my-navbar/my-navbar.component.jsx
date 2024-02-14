d984841ce1c65af13bb1c423ae8203afd716550f
Vutukuri Sathvik <savutukuri@paypal.com>
2024-02-14 15:35:08 +0530

    neww

--- src/components/my-navbar/my-navbar.component.jsx
+++ src/components/my-navbar/my-navbar.component.jsx
@@ -8,11 +8,7 @@ const MyNavbar = () => {
 	return (
 		<div>
 			<Navbar fixed="top" variant="dark" expand="md" className="animate-navbar nav-theme justify-content-between">
-				<div>
-					<Navbar.Brand href="#home">
-						<img className="logo" src={Logo} alt="NO_IMAGE" />
-					</Navbar.Brand>
-				</div>
+				
 				<div>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
@@ -21,7 +17,6 @@ const MyNavbar = () => {
 							<Nav.Link href="#about">About</Nav.Link>
 							<Nav.Link href="#skills">Skills</Nav.Link>
 							<Nav.Link href="#experience">Experience</Nav.Link>
-							{/*<Nav.Link href="#projects">Projects</Nav.Link>*/}
 							<Nav.Link href="#contact">Contact</Nav.Link>
 						</Nav>
 					</Navbar.Collapse>
