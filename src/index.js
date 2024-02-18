d9c77a6b6ef7389f3605d01f3d66e1a582573f88
Vutukuri Sathvik <savutukuri@paypal.com>
2024-02-18 10:29:53 +0530

    latests

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from "styled-components";
 import { theme } from "./themes/theme";
 import 'semantic-ui-css/semantic.min.css'
 ReactDOM.render(
   
   <ThemeProvider theme={theme}>
-    <BrowserRouter basename="/prathyushanikhilwedding">
+    <HashRouter basename="/prathyushanikhilwedding">
     <App />
-    </BrowserRouter>
+    </HashRouter>
   </ThemeProvider>,
   document.getElementById("root")
 );
