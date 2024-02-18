292e99013527a8fcca8fc8ae39aa7409e4fb48b3
Vutukuri Sathvik <savutukuri@paypal.com>
2024-02-18 16:55:29 +0530

    send la

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter,BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from "styled-components";
 import { theme } from "./themes/theme";
 import 'semantic-ui-css/semantic.min.css'
