b4ceeaf40a7f52db9a6d25ef4db490d732283b2a
Vutukuri Sathvik <savutukuri@paypal.com>
2024-02-20 07:52:42 +0530

    latest

--- src/components/my-carousal/my-carousal.component.jsx
+++ src/components/my-carousal/my-carousal.component.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import Slide1 from "../../assets/img/carousal/Slide1-modified.jpeg";
-import Slide2 from "../../assets/img/carousal/slide2.webp";
-import Slide4 from "../../assets/img/skills/slide_4.jpeg";
-import Slide6 from "../../assets/img/skills/slide_6.jpeg";
-import Slide5 from "../../assets/img/skills/slide6.jpeg";
-import Slide9 from "../../assets/img/skills/Slide9.jpeg";
-import Slide8 from "../../assets/img/skills/slide_8.jpeg";
+import Slide2 from "../../assets/img/carousal/Slide1-modified-compressed.webp";
+import Slide1 from "../../assets/img/carousal/slide2.webp";
+import Slide4 from "../../assets/img/carousal/slide_4.webp";
+import Slide5 from "../../assets/img/carousal/slide_6.webp";
+import Slide6 from "../../assets/img/carousal/slide6.webp";
+import Slide3 from "../../assets/img/carousal/Slide9.webp";
+import Slide8 from "../../assets/img/carousal/slide_8.webp";
 
 import ScrollDown from "../scroll-down/scroll-down.component";
 import "./my-carousal.styles.css";
@@ -16,32 +16,34 @@ const MyCarousal = () => {
     <div id="home">
       <Carousel controls={false} indicators interval={2500} pause={false}>
         <Carousel.Item>
-          <img className="d-block w-100 custom-img" src={Slide2} alt="First slide" />
+          <img className="d-block w-100 custom-img" src={Slide1} alt="First slide" />
           
         </Carousel.Item>
        
         <Carousel.Item>
-          <img className="d-block w-100 custom-img" src={Slide1} alt="Third slide" />
+          <img className="d-block w-100 custom-img" src={Slide2} alt="Third slide" />
          
         </Carousel.Item>
 
+        <Carousel.Item>
+          <img className="d-block w-100 custom-img" src={Slide3} alt="Third slide" />
+         
+        </Carousel.Item>
          <Carousel.Item>
           <img className="d-block w-100 custom-img" src={Slide4} alt="Third slide" />
          
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100 custom-img" src={Slide9} alt="Third slide" />
+          <img className="d-block w-100 custom-img" src={Slide5} alt="Third slide" />
          
         </Carousel.Item>
+       
         <Carousel.Item>
           <img className="d-block w-100 custom-img" src={Slide6} alt="Third slide" />
          
         </Carousel.Item>
        
-        <Carousel.Item>
-          <img className="d-block w-100 custom-img" src={Slide5} alt="Third slide" />
-         
-        </Carousel.Item>
+      
         <Carousel.Item>
           <img className="d-block w-100 custom-img" src={Slide8} alt="Third slide" />
          
